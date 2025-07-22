8811599a0923fbf3bdb773a48752ec01144f3903
lqw <lqw350128>
2025-07-22 12:52:02 +0800

    修改节点图标与背景色，统一字体

--- app/components/base/icons/src/vender/workflow/IfElse.tsx
+++ app/components/base/icons/src/vender/workflow/IfElse.tsx
@@ -2,7 +2,7 @@
 // DON NOT EDIT IT MANUALLY
 
 import * as React from 'react'
-import data from './IfElse.json'
+import data from './IfElse_Funo.json'
 import IconBase from '@/app/components/base/icons/IconBase'
 import type { IconBaseProps, IconData } from '@/app/components/base/icons/IconBase'
 
